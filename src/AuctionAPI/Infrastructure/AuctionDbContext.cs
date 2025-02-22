f1879c0403d36389c25845089c5721cadd2de86d
Akadnt <nhatt@DESKTOP-9059ERI>
2025-02-22 16:24:39 +0700

    Convert Enum

--- src/AuctionAPI/Infrastructure/AuctionDbContext.cs
+++ src/AuctionAPI/Infrastructure/AuctionDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AuctionAPI.Entities;
+using AuctionAPI.Enums;
 using Microsoft.EntityFrameworkCore;
 
 namespace AuctionAPI.Infrastructure
@@ -13,5 +14,15 @@ namespace AuctionAPI.Infrastructure
         {
         }
         public DbSet<Auction> Auctions { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Auction>()
+                .Property(a => a.Status)
+                .HasConversion(
+                    v => v.ToString(),  // Chuyển giá trị enum thành chuỗi
+                    v => (Status)Enum.Parse(typeof(Status), v)  // Chuyển chuỗi thành enum
+                );
+        }
+
     }
 }
