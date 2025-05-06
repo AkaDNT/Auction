b5afd90f63b6659e486314f2864e4ec5a3de5cfc
Akadnt <nhatthong346@gmail.com>
2025-05-07 00:35:34 +0700

    Add get me endpoint

--- src/AuctionAPI/Controllers/AccountController.cs
+++ src/AuctionAPI/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 
 using AuctionAPI.DTOs;
+using AuctionAPI.Identity;
 using AuctionAPI.Services;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AuctionAPI.Controllers
@@ -12,10 +14,12 @@ namespace AuctionAPI.Controllers
     public class AccountController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public AccountController(IMediator mediator)
+        public AccountController(IMediator mediator, UserManager<ApplicationUser> userManager)
         {
             _mediator = mediator;
+            _userManager = userManager;
         }
 
         [AllowAnonymous]
@@ -24,6 +28,22 @@ namespace AuctionAPI.Controllers
         {
             await _mediator.Send(new UserRegisterService.Command { userRegister = userRegister });
             return Ok();
+        }
+
+        [AllowAnonymous]
+        [HttpGet("me")]
+        public async Task<ActionResult> GetMe()
+        {
+            if (User.Identity.IsAuthenticated == false) return NoContent();
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null) return Unauthorized();
+            return Ok(new
+            {
+                user.DisplayName,
+                user.Email,
+                user.Id,
+                user.ImageUrl
+            });
         }
 
     }
