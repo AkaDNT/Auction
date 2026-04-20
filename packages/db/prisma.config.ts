6a5b4afe9d3987aec8e0a34f4632590f44139e8e
AkaDNT <nhatthong346@gmail.com>
2026-04-21 00:22:31 +0700

    fix: seed data

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    seed: "tsx src/run-seed.ts",
+    seed: "node dist/run-seed.js",
   },
   engine: "classic",
   datasource: {
